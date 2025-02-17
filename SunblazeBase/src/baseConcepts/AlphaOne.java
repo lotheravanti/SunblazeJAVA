3b6196ceeaaef69b86e2b1e85e5501881fafce2f
lothergrabcad <lotheravanti@yahoo.com>
2025-02-17 18:22:34 +0200

    Initial setup

---
+++ SunblazeBase/src/baseConcepts/AlphaOne.java
@@ -0,0 +1,11 @@
+package baseConcepts;
+
+public class AlphaOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Alpha One Initialized");
+
+	}
+
+}
