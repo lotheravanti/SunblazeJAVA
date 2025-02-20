bba4b6979872d7c2f39942656ef973454f37baac
lothergrabcad <lotheravanti@yahoo.com>
2025-02-20 20:20:54 +0200

    Update AlphaOne.java

--- SunblazeBase/src/baseConcepts/AlphaOne.java
+++ SunblazeBase/src/baseConcepts/AlphaOne.java
@@ -19,14 +19,39 @@ public class AlphaOne {
 		//Non Primitives
 		//Double quotes for String
 		String message = "Alpha One Initialized";
-		String lowerCase = "lower case text";
-		String replacedMessage = message.replace("One", "Prime");
+		//Arrays
+		int[] integerArray = {1, 2, 3, 4, 5, 6, 7};
+		String[] stringArray = {"This", "is", "an", "Array", "from", "a", "String."};
+		//Implicit variable declaration can only be done inside a Method(main here)
+		var varString = "varString";
+		var varInteger = new int[]{1, 2, 3, 4, 5};
+		
+		//String Operations
+		String stringValue = "lower case text";
+		//Get length of String
+		int lengthString = message.length();
 		//Convert to Upper Case
-		String upperCase = lowerCase.toUpperCase();
+		String upperCase = stringValue.toUpperCase();
+		//Reverse String
+		//Replace part of String
+		String replacedMessage = message.replace("One", "Prime");
+		//Get last N characters of a String
+		//Remove First and Last characters of String
+		//Check if String is Alphabet
+		//Verify if String starts or ends with
 		boolean startsWith = message.startsWith("Alpha");
 		boolean endsWith = message.endsWith("Initialized");
-		int[] integerArray = {1,1};
-		String[] stringArray = {"First", "Second"};
+		//Check if String is Upper Case or Lower Case
+		
+		//Array Operations
+		//Create new placeholder Array of fixed length
+		int[] fixedArray = new int[5];
+		fixedArray[2] = 10;
+		//Create Two Dimensional Array
+		int[][] twoDimArray = {{1, 2}, {3, 4}, {5, 6}};
+		//Generate String from Array with delimiter
+		//Reverse Array
+		
 		
 		System.out.print(replacedMessage + " starts with Alpha: " + startsWith + " and ends with Initialized: " + endsWith);
 
