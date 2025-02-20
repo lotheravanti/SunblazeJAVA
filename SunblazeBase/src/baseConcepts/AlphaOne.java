679fc609662c34d5cb01341655c09441642fb085
lothergrabcad <lotheravanti@yahoo.com>
2025-02-20 16:30:19 +0200

    Update AlphaOne.java

--- SunblazeBase/src/baseConcepts/AlphaOne.java
+++ SunblazeBase/src/baseConcepts/AlphaOne.java
@@ -4,7 +4,31 @@ public class AlphaOne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Alpha One Initialized");
+		
+		//Primitives
+		int integer = 10;
+		double decimal = 12.45;
+		float realNumber = 3.1417639f;
+		long largeNumber = 100000000000L;
+		//Max value for byte is 100
+		byte binaryValue = 100;
+		boolean booleanValue = true;
+		//Single quotes for Character
+		char character = 'a';
+		
+		//Non Primitives
+		//Double quotes for String
+		String message = "Alpha One Initialized";
+		String lowerCase = "lower case text";
+		String replacedMessage = message.replace("One", "Prime");
+		//Convert to Upper Case
+		String upperCase = lowerCase.toUpperCase();
+		boolean startsWith = message.startsWith("Alpha");
+		boolean endsWith = message.endsWith("Initialized");
+		int[] integerArray = {1,1};
+		String[] stringArray = {"First", "Second"};
+		
+		System.out.print(replacedMessage + " starts with Alpha: " + startsWith + " and ends with Initialized: " + endsWith);
 
 	}
 
