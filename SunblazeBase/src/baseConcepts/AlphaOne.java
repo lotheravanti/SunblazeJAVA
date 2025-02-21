913c6499441839649672c11d1c4157270b1f85b5
lothergrabcad <lotheravanti@yahoo.com>
2025-02-21 17:08:10 +0200

    Update AlphaOne.java

--- SunblazeBase/src/baseConcepts/AlphaOne.java
+++ SunblazeBase/src/baseConcepts/AlphaOne.java
@@ -1,5 +1,8 @@
 package baseConcepts;
 
+import java.util.*;
+import java.util.regex.Pattern;
+
 public class AlphaOne {
 
 	public static void main(String[] args) {
@@ -26,6 +29,12 @@ public class AlphaOne {
 		var varString = "varString";
 		var varInteger = new int[]{1, 2, 3, 4, 5};
 		
+		//Integer Operations
+		//Split String into Integer Array, split to String Array(with REGEX), then convert into Integer Array
+		String stringInt = "549713";
+		String[] stringToArray = stringInt.split("(?<=.)");
+		int[] stringToIntArray = Arrays.stream(stringToArray).mapToInt(Integer::parseInt).toArray();
+		
 		//String Operations
 		String stringValue = "lower case text";
 		//Get length of String
@@ -33,28 +42,73 @@ public class AlphaOne {
 		//Convert to Upper Case
 		String upperCase = stringValue.toUpperCase();
 		//Reverse String
+		StringBuilder reverseString = new StringBuilder().append(stringValue).reverse();
 		//Replace part of String
 		String replacedMessage = message.replace("One", "Prime");
 		//Get last N characters of a String
+		String lastOfString = stringValue.substring(stringValue.length() - 3);
 		//Remove First and Last characters of String
+		String removeFirstLast = stringValue.substring(1, stringValue.length() - 1);
 		//Check if String is Alphabet
+		String stringAlphabet = "OnLyAlPhAbEt"; 
+		boolean checkAlphabet = Pattern.matches("[a-zA-Z]+",stringAlphabet);
 		//Verify if String starts or ends with
 		boolean startsWith = message.startsWith("Alpha");
 		boolean endsWith = message.endsWith("Initialized");
 		//Check if String is Upper Case or Lower Case
+		String upperString = "ALLCAPS";
+		boolean isUpper = upperString.equals(upperString.toUpperCase());
+		String lowerString = "alllower";
+		boolean isLower = lowerString.equals(lowerString.toLowerCase());
 		
 		//Array Operations
+		//Get length of Array
+		int lengthArray = stringArray.length;
 		//Create new placeholder Array of fixed length
 		int[] fixedArray = new int[5];
 		fixedArray[2] = 10;
 		//Create Two Dimensional Array
 		int[][] twoDimArray = {{1, 2}, {3, 4}, {5, 6}};
 		//Generate String from Array with delimiter
-		//Reverse Array
+		String joinedArray = String.join(" ", stringArray);
+		//Reverse Array method in JAVA permanently reverses Array, so can't use original
+		String[] reverseArray = stringArray;
+		Collections.reverse(Arrays.asList(reverseArray));
 		
+		//If statement
+		boolean condition = false;
+		if (integerArray[0] == 1){
+		      condition = true;
+		    }
+		else {
+			condition = false;
+		}
+		//If statement with AND
+		if (integerArray[0] == 1 && integerArray[1] == 2){
+		      condition = true;
+		    }
+		//If statement with OR
+		if (integerArray[0] == 1 || integerArray[1] == 2){
+		      condition = true;
+		    }
 		
-		System.out.print(replacedMessage + " starts with Alpha: " + startsWith + " and ends with Initialized: " + endsWith);
-
+		//Integers
+		System.out.println("Split '" + stringInt + "' to Int Array '" + Arrays.toString(stringToIntArray) + "'");
+		
+		//Strings
+		//No F String method exists for JAVA
+		System.out.println("String length for '" + stringValue + "' is " + lengthString);
+		System.out.println("Reversed String is '" + reverseString + "'");
+		System.out.println("Stripping last 3 characters from '" + stringValue + "' results in '" + lastOfString + "'");
+		System.out.println("Removing first and last characters from '" + stringValue + "' results in '" + removeFirstLast + "'");
+		System.out.println("'" + stringAlphabet + "' contains only alphabet characters: " + checkAlphabet);
+		System.out.println(replacedMessage + " starts with Alpha: " + startsWith + " and ends with Initialized: " + endsWith);
+		System.out.println("'" + upperString + "' is all Upper Case: " + isUpper);
+		System.out.println("'" + lowerString + "' is all Lower Case: " + isLower);
+		
+		//Arrays
+		System.out.println("Joined Array is '" + joinedArray + "'");
+		System.out.println("Reversed Array is '" + String.join(" ", reverseArray) + "'");
 	}
 
 }
